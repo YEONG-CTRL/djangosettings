049d3016cb768245ccbe4c100b80fbd24854a506
Park YengSeo <1park4170@gmail.com>
2022-02-11 10:01:15 +0900

    it is prestudy2

--- products/models.py
+++ products/models.py
@@ -1,3 +1,4 @@
+from tkinter import CASCADE
 from unicodedata import category
 from django.db import models
 
@@ -9,13 +10,13 @@ class Menu(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=45)
-    menu = models.ForeignKey('Menu', null=True ,on_delete=models.CASCADE) #null=True means 비어있으면 안됨
+    menu = models.ForeignKey('Menu', on_delete=models.CASCADE) #null=True means 비어있어도 괜찮음
     class Meta:
         db_table = 'categories'
 
 class images(models.Model):
     image_url = models.CharField(max_length=300)
-    drink = models.ForeignKey('Drinks', null=True ,on_delete=models.CASCADE)
+    drink = models.ForeignKey('Drinks', on_delete=models.CASCADE)
     class Meta:
         db_table = 'images'
 
@@ -23,30 +24,31 @@ class Drinks(models.Model):
     korean_name = models.CharField(max_length=45)
     english_name = models.CharField(max_length=45)
     description = models.TextField(max_length=100)
-    category = models.ForeignKey('Category', null = True ,on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
     class Meta:
         db_table = 'drinks'
 
-class Allergy_drink(models.Model):
-    allergy_id = models.IntegerField()
-    drink = models.ForeignKey('Drinks',on_delete=models.CASCADE)
+class Allergy_drink(models.Model):     #drinks와 allergy 의 중간테이블 
+    allergy = models.ForeignKey('Allergy', on_delete=models.CASCADE)
+    drink = models.ForeignKey('Drinks', on_delete=models.CASCADE)
     class Meta:
         db_table = 'allergy_drinks'
-    #얘는 drinks와 allergy 의 중간테이블 
 
-class allergy(models.Model):
+
+class Allergy(models.Model):
     name = models.CharField(max_length=45)
+    drinks = models.ManyToManyField(Drinks, through='Allergy_drink') #through에 중간테이블명 넣어줌
     class Meta:
         db_table = 'allergies'
     #다대다 관계일때 FK 비어있어도 돼서 null=True 안해줘도 됨
 
-class Nutritions(models.Model):
+class Nutritions(models.Model):   #drinks와 sizes의 중간테이블 
     one_serving_kcal = models.DecimalField(max_digits=10, decimal_places=5)
     sodium_mg = models.DecimalField(max_digits=10, decimal_places=5)
     saturated_fat_g = models.DecimalField(max_digits=10, decimal_places=5)
     sugars_g = models.DecimalField(max_digits=10, decimal_places=5)
     protein_g = models.DecimalField(max_digits=10, decimal_places=5)
-    drink = models.ForeignKey('Drinks',on_delete=models.CASCADE)
+    drink = models.ForeignKey('Drinks', on_delete=models.CASCADE )
     size = models.ForeignKey('Sizes', on_delete=models.CASCADE)
     class Meta:
         db_table = 'nutritions'
@@ -55,6 +57,7 @@ class Sizes(models.Model):
     name = models.CharField(max_length=45)
     size_mi = models.CharField(max_length=45)
     size_fluid_ounce = models.CharField(max_length=45)
+    drinks = models.ManyToManyField(Drinks, through='nutritions') #through에 중간테이블명 넣어줌
     class Meta:
         db_table = 'sizes'
 
@@ -64,3 +67,4 @@ class Sizes(models.Model):
 # drinks(다) - (다) allergy : many-to-many / allergy_drink 중간테이블
 # drinks(다) - (다)sizes  : many-to-many / nutritions 중간테이블 
 # NULL은 포인터가 가져올 값이 없는 상태
+# 중간테이블 직접 정의 through model
